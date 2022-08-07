f28ef5c36fdd56b170907af01a0b3e1962d67e37
AmrAwad88 <awad.amr1988@gmail.com>
2022-08-07 14:12:27 +0200

    Lists Methods

--- 6.Lists.py
+++ 6.Lists.py
@@ -47,3 +47,170 @@ myAwesomeList[0:4] = []             # delete
 print(myAwesomeList)
 #myAwesomeList[4] = 5               # add is not possible this way
 #print(myAwesomeList)
+
+# -------------------------------------------------------------------------------------------------------------
+# Lists Methods:
+# 1.  apend(element)
+#   appends an element by the end of the list, the appended element is added as just one element
+#   the element type can be: int, float, string, complex or list
+# 2.  insert(index, object)
+#   inserts the provided object before index, the inserted element is added as just one element
+#   the element type can be: int, float, string, complex or list
+# 3.  extend(element)
+#   extends the original list with that given arguement (element)
+#   the element type can be: array and each element is added individually
+#                            or 
+#                            string, each letter is considered as a signle element
+# 4.  remove(element content)
+#   removes the given element content from the whole list (int, float, complex or string)
+# 5.  pop(index)
+#   removes and returns item of that index
+#
+# 6.  sort(reverse = True/False)
+#   sorts numerrical or string (not both together) values in ascending or descending order
+#       (default) reverse = False -> Ascending order
+#       reverse = True -> Descending order
+# 7.  reverse()
+#   reverses the list
+#
+# 8.  clear()
+#   clears the list
+# 9.  copy()
+#   returns a shallow copy of the list
+#
+# 10. count(element)
+#   returns how many occurancies of the element are found in the list
+# 11. count(element)
+#   returns the first index of the element found in the list
+# -------------------------------------------------------------------------------------------------------------
+
+print('\n# ********************************************* #')
+print('LISTS METHODS:\n')
+print('apend method:')
+myNames = ["Amr"]
+print(myNames)
+
+myNames.append("Wael")
+print(myNames)
+
+myNames.append("Ali")
+print(myNames)
+
+myNames.append(["Awad", "Gaafer"])
+print(myNames)
+print(myNames[1])
+print(type(myNames[1]))
+print(myNames[3])
+print(type(myNames[3]))
+print(myNames[3][0])
+print(myNames[3][1])
+print(type(myNames[3][1]))
+print('# --------------------------------------------- #')
+
+print('insert method:')
+myNames = ['Wael', 'Gaafer']
+myNames.insert(0, "Amr")
+myNames.insert(2, ["Ali", "Awad"])
+print(myNames)
+print('# --------------------------------------------- #')
+
+print('extend method:')
+myNames = ["Amr"]
+print(myNames)
+myNames.extend(["Wael"])
+print(myNames)
+myNames.extend(["Ali"])
+print(myNames)
+myNames.extend(["Awad", "Gaafer"])
+print(myNames)
+
+myNumbers = [1 , 2, 3]
+print(myNumbers)
+myNumbers.extend([4])
+print(myNumbers)
+myNumbers.extend([5, 6])
+print(myNumbers)
+print('# --------------------------------------------- #')
+
+print('remove method:')
+myNames = ['Amr', 'Wael', 'Ali', 'Awad', 'Gaafer']
+myNames.extend([110388])
+print(myNames)
+myNames.remove(110388)
+print(myNames)
+myNames.remove("Gaafer")
+print(myNames)
+print('# --------------------------------------------- #')
+
+print('pop method:')
+myNames = ['Amr', 'Wael', 'Ali', 'Awad', 'Gaafer']
+myName = myNames.pop(0)
+print(myName)
+print(myNames)
+
+print('# --------------------------------------------- #')
+
+print('sort method:')
+myNumbers = [-100, 15.7, 26, 0, -2.31, -1024 , 650000]
+print(myNumbers)
+myNumbers.sort()
+print(myNumbers)
+myNumbers.sort(reverse= False)
+print(myNumbers)
+myNumbers.sort(reverse= True)
+print(myNumbers)
+
+myNames.sort()
+print(myNames)
+myNames.sort(reverse= True)
+print(myNames)
+print('# --------------------------------------------- #')
+
+print('reverse method:')
+myNumbers = [-100, 15.7, 26, 0, -2.31, -1024 , 650000]
+print(myNumbers)
+myNumbers.reverse()
+print(myNumbers)
+
+myNames = ['Amr', 'Wael', 'Ali', 'Awad']
+print(myNames)
+myNames.reverse()
+print(myNames)
+print('# --------------------------------------------- #')
+
+print('clear method:')
+myNumbers = [-100, 15.7, 26, 0, -2.31, -1024 , 650000]
+print(myNumbers)
+myNumbers.clear()
+print(myNumbers)
+
+myNames = ['Amr', 'Wael', 'Ali', 'Awad']
+print(myNames)
+myNames.clear()
+print(myNames)
+print('# --------------------------------------------- #')
+
+print('copy method:')
+a = [0, 1, 2 , 3, 4]
+b = a.copy()
+print(a)
+print(b)
+
+a.append(5)
+print(a)
+print(b)
+print('# --------------------------------------------- #')
+
+print('count method:')
+myNumbers = [-100, 15.7, 26, 0, -2.31, -1024 , 650000]
+print(myNumbers.count(15.7))
+print(myNumbers.count(0.0))
+print(myNumbers.count(0))
+print('# --------------------------------------------- #')
+
+print('index method:')
+myNames = ['Amr', 'Wael', 'Ali', 'Awad']
+print(myNames.index("Awad"))
+myNumbers = [-100, 15.7, 26, 0, -2.31, -1024 , 650000]
+print(myNumbers.index(0.0))
+print('# --------------------------------------------- #')
