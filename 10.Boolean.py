8f1f576a2e5b3b73b2b0733793072e90efb0be01
AmrAwad88 <awad.amr1988@gmail.com>
2022-08-14 13:59:27 +0200

    Boolean

---
+++ 10.Boolean.py
@@ -0,0 +1,32 @@
+# -------------------------------------------------------------------------------------------------------------
+# Boolean:
+# logical variable: True or False
+# Type Casting:
+#   to boolean:     bool(var), True if not empty or has a numirical value other than 0
+# Boolean Operators:
+#   and, or, not
+# -------------------------------------------------------------------------------------------------------------
+
+print('\n# ********************************************* #')
+print('BOOLEAN:\n')
+
+print(bool(True))
+print(bool(False))
+
+print(bool(100))
+print(bool(0))
+
+print(bool(1))
+print(bool(None))
+
+print(bool(100.0))
+print(bool(0.0))
+
+print(bool(100.0+5j))
+print(bool(0.0+0.0j))
+
+print(bool("Amr"))
+print(bool(""))
+
+print(bool([1,0]))
+print(bool([]))
