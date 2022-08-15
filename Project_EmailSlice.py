a0455681409f1cba5567fe98e1194c4efb4df702
AmrAwad88 <awad.amr1988@gmail.com>
2022-08-15 23:54:26 +0200

    Project: E-Mail Slice

---
+++ Project_EmailSlice.py
@@ -0,0 +1,11 @@
+# -------------------------------------------------------------------------------------------------------------
+# Project: E-Mail Slice
+# -------------------------------------------------------------------------------------------------------------
+
+name = input("What is your name? ").strip().capitalize()
+email = input("What is your email? ").strip().lower()
+
+user =   email[:email.index('@')]
+domain = email[email.index('@')+1 : email.find(".",email.index('@'))]
+
+print(f"Hi {name}, your registered E-Mail is: {email}\nYour username is {user}\nyour domain is {domain}")
