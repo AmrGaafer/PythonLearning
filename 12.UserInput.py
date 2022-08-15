da925a60b67ba86a91f83a90db98e19b0e414a05
AmrAwad88 <awad.amr1988@gmail.com>
2022-08-15 22:00:14 +0200

    User Input

---
+++ 12.UserInput.py
@@ -0,0 +1,16 @@
+# -------------------------------------------------------------------------------------------------------------
+# User Input:
+# input(message)    receives the user input from the terminal and sends it to the assigned parameter
+#
+# NOTE: Python supports methods chain
+# -------------------------------------------------------------------------------------------------------------
+
+firstName = input("what is your first name? ")
+secondName = input("what is your first name? ")
+lastName = input("what is your first name? ")
+
+firstName = firstName.strip().capitalize()
+secondName = secondName.strip().capitalize()[0]+"."
+lastName = lastName.strip().capitalize()
+
+print(f"Hello {firstName} {secondName} {lastName}!")
