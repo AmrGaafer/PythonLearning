eb6b51efd5a9990d9ab2d6b3bf3a3307fa4510aa
AmrAwad88 <awad.amr1988@gmail.com>
2022-08-16 14:26:14 +0200

    Project: Your Age Full Details

---
+++ Project_YourAgeFullDetails.py
@@ -0,0 +1,19 @@
+# -------------------------------------------------------------------------------------------------------------
+# Project: Your Age Full Details
+# -------------------------------------------------------------------------------------------------------------
+
+# Input age
+age = int(input("What is your age? ").strip())
+print(age)
+print(type(age))
+
+# Get age in all time units
+months = age * 12
+weeks = months * 4
+days = age * 365
+hours = days * 24
+minutes = hours * 60
+seconds = minutes * 60
+
+print("You lived for: ")
+print(f"{months} Months\n{weeks:,} Weeks\n{days:,} Days\n{hours:,} Hours\n{minutes:,} Minutes\n{seconds:,} Seconds")
