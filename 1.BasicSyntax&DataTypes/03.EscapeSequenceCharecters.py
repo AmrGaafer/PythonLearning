0a50bab8c9827289f4231699b6d8007b158df79c
AmrAwad88 <awad.amr1988@gmail.com>
2022-08-22 18:49:47 +0200

    raw string r operator

--- 1.BasicSyntax&DataTypes/03.EscapeSequenceCharecters.py
+++ 1.BasicSyntax&DataTypes/03.EscapeSequenceCharecters.py
@@ -10,6 +10,8 @@
 # NOTE: \n\r guarantees that the following comes in a new line without any other precedent characters
 # \t:   Horizontal Tab
 # \xhh: Character with a Hex Value (ASCII Code)
+#
+# NOTE: to escape the escape sequence characters, use raw string "r" operator before the string
 # -------------------------------------------------------------------------------------------------------------
 
 # Backspace:
@@ -39,3 +41,32 @@ print("Hello\tWorld\twith\tTabs")
 
 # Character with a Hex Value:
 print("\x48\x65\x6C\x6C\x6F\x20\x57\x6F\x72\x6C\x64\x20\x69\x6E\x20\x41\x53\x43\x49\x49")
+
+print("\nPrint raw string using thr \"r\" operator:")
+# Backspace:
+print(r"Hello  \bWorld!\b ")
+
+# Escape New Line:
+print(r"Hello \
+World")
+
+# Escape Backslash:
+print(r"I love Backslash, here it is: \\")
+
+# Escape Single Quote:
+print(r'I\'m Amr')
+
+# Escape Double Quotes:
+print(r"And I love escaping the \"double\" quotes")
+
+# New Line:
+print(r"Hello\nWorld")
+
+# Carriage Return:
+print(r"12345678901234567890\rCarriage Return")
+
+# Horizontal Tab:
+print(r"Hello\tWorld\twith\tTabs")
+
+# Character with a Hex Value:
+print(r"\x48\x65\x6C\x6C\x6F\x20\x57\x6F\x72\x6C\x64\x20\x69\x6E\x20\x41\x53\x43\x49\x49")
