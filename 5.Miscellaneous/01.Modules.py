4a36803e9c747faa4ef32c7edfc69fcef8621f6b
AmrAwad88 <awad.amr1988@gmail.com>
2022-09-13 22:10:02 +0200

    Modules Part 3 - Install External Packages

--- 5.Miscellaneous/01.Modules.py
+++ 5.Miscellaneous/01.Modules.py
@@ -11,6 +11,7 @@
 #       Hint #1: if the whole module is imported, the function can be called only using the resolution operator (.)
 #                (e.g. random.random(), functools.reduce())
 #       import moduleName as moduleAlias        imports the module as an alias
+#       Built-in Modules list: https://docs.python.org/3/py-modindex.html
 #
 #     2. Function import:
 #       from moduleName import fn1, fn2,...     imports certain function(s) from the module
@@ -20,6 +21,19 @@
 #
 #       dir(moduleName)                         returns all the available functions in that module
 #                                               !! The module has to be previously imported !!
+#   Install external packages:
+#       1. Module is a single file, Package is group of Modules
+#       2. downloadable from the internet
+#       3. Python Package Manager PIP is the tool to install Packages and their dependencies
+#       External Packages list: https://pypi.org/
+#       pip documentation: https://pip.pypa.io/en/stable/
+#   Syntax (on Terminal):
+#       pip --version                           returns the version of the installed pip
+#       pip list                                returns a list of all installed pip
+#       pip install PackageName                 installs the given Package
+#       pip install PackageName==x.x.x          installs the given Package version x.x.x
+#       pip install PackageName>=x.x.x          installs the given Package version x.x.x or newer
+#       pip install pip --upgrade pip           upgrades pip
 # -------------------------------------------------------------------------------------------------------------
 
 print('\n# ********************************************* #')
@@ -47,3 +61,10 @@ import MyModule
 print(dir(MyModule))
 MyModule.sayHello('Amr')
 MyModule.sayHowAreYou('Amr')
+
+print('\n# ********************************************* #')
+print('Import External Modules:\n')
+import termcolor    #(on Terminal) pip install termcolor
+import pyfiglet     #(on Terminal) pip install pyfiglet
+
+print(termcolor.colored(pyfiglet.figlet_format('Amr'), color='magenta'))
