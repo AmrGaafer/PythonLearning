142e3bd1be47f1ff071a62d2de5e5eacb808dfd6
AmrAwad88 <awad.amr1988@gmail.com>
2022-09-13 16:48:00 +0200

    Modules Part 2 - Create Your Module

--- 5.Miscellaneous/01.Modules.py
+++ 5.Miscellaneous/01.Modules.py
@@ -1,20 +1,31 @@
 # -------------------------------------------------------------------------------------------------------------
-# Modules: File that contains a set of funcitons
+# Module: .py File that contains a set of functions
 #   1. can be either built-in or user defined
 #   2. can be imported in the application (one or multiple)
 #
 #   NOTE: Built-in modules are saved here:
 #           C:\Users\user\AppData\Local\Programs\Python\Python39\Lib
 #   Syntax:
+#     1. Module import:
 #       import moduleName1, moduleName2         imports the whole module(s)
-#       Hint: if the whole module is called, the function can be called using the resolution operator (.)
-#             e.g. random.random(), functools.reduce()
+#       Hint #1: if the whole module is imported, the function can be called only using the resolution operator (.)
+#                (e.g. random.random(), functools.reduce())
+#       import moduleName as moduleAlias        imports the module as an alias
+#
+#     2. Function import:
 #       from moduleName import fn1, fn2,...     imports certain function(s) from the module
+#       Hint #2: if certain function(s) of module is/are imported, 
+#                the function can be called without using the resolution operator (.)(e.g. random(), reduce())
+#       from moduleName import fn1 as fnAlias   imports certain function as an alias from the module
+#
 #       dir(moduleName)                         returns all the available functions in that module
 #                                               !! The module has to be previously imported !!
 # -------------------------------------------------------------------------------------------------------------
 
+print('\n# ********************************************* #')
+print('Built-in Modules:\n')
 import random, functools
+
 print(f'print a random number {random.random()}')
 print(f'try a second module {functools.reduce(lambda a, b: a+b, [1 , 2, 3])}')
 
@@ -22,3 +33,17 @@ from random import randint
 print(f'print a random integer number {randint(-10,100)}')
 
 print(dir(random))
+
+print('\n# ********************************************* #')
+print('User-defined Modules:\n')
+import sys
+import os
+print(sys.path)
+print(os.path.dirname(os.path.abspath(__file__)))
+#sys.path.append(os.path.dirname(os.path.abspath(__file__)))    # NOTE: one time call
+#print(sys.path)
+
+import MyModule
+print(dir(MyModule))
+MyModule.sayHello('Amr')
+MyModule.sayHowAreYou('Amr')
