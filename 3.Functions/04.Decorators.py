27e2c51fede968c670493153d81651d21376194a
AmrAwad88 <awad.amr1988@gmail.com>
2022-09-27 17:26:32 +0200

    Decorators - Function With Parameters

--- 3.Functions/04.Decorators.py
+++ 3.Functions/04.Decorators.py
@@ -4,7 +4,13 @@
 #   - wrap other functions and enhance their behaviour (take a function argument and returns it with added functionality)
 #   Syntax: Sugar Syntax Method:
 #           @Decorator_function
-#           def function_ToBeDocarated():...
+#           def function_DecoratingFunction():...
+#               ...
+#               function_ToBeDocarated()
+#               ...
+#
+#           - function_DecoratingFunction() has to pass the paramters needed to the wraped function
+#           - function_ToBeDocarated()      has to have matching blueprint to to wrapped function
 #
 # NOTE: Everything in Python is an object, even functions
 # -------------------------------------------------------------------------------------------------------------
@@ -12,8 +18,10 @@
 import os
 os.system('cls')        # cls command
 
-def myDecorator(fn):    # Decorator accepts fn()
-    def nested_fn():
+print('\n# ********************************************* #')
+print('Functions without parameters decoration:')
+def myDecorator(fn):    # decorator accepts fn()
+    def nested_fn():    # decorating function (wraper function)
         print('before')
         fn()
         print('after')
@@ -32,3 +40,29 @@ def SayHelloSugarSyntax():
     print('Hello from function with sugar syntax!')
 
 SayHelloSugarSyntax()
+
+print('\n# ********************************************* #')
+print('Functions with parameters decoration:')
+def myDecorator(fn):
+    def nested_fn(n1, n2):  # has to pass the parameters needed to the wrapped function
+        print('before')
+        if n1 < 0 or n2 < 0:
+            print('One or both numbers is less than zero')
+        fn(n1, n2)          # has to have matching blueprint to to wrapped function
+        print('after')
+    return nested_fn
+
+def myDecorator2(fn):
+    def nested_fn(n1, n2):  # has to pass the parameters needed to the wrapped function
+        print('Decorator2')
+        fn(n1, n2)          # has to have matching blueprint to to wrapped function
+    return nested_fn
+
+@myDecorator
+@myDecorator2
+def calculate(n1, n2):
+    print(n1 + n2)
+
+calculate(1, 2),
+print('-----------------------')
+calculate(-1, 2)
