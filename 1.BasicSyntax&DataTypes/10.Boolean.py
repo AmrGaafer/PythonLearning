fa7d3e23e3182ffbfb8dd1fb26257433c2b2ac84
AmrAwad88 <awad.amr1988@gmail.com>
2022-10-21 11:03:09 +0200

    bitwise operators

--- 1.BasicSyntax&DataTypes/10.Boolean.py
+++ 1.BasicSyntax&DataTypes/10.Boolean.py
@@ -5,6 +5,8 @@
 #   bool(var):     to boolean, True if not empty or has a numirical value other than 0
 # Boolean Operators:
 #   and, or, not
+# Bitwise Operators:
+#   <<(left shift), >>(right shift), &, |, ^(XOR), and ~(-x-1)
 # Membership Operators:
 #   in, not in
 #   Syntax: x in y | x not in y
@@ -37,6 +39,15 @@ print(bool(""))
 
 print(bool([1,0]))
 print(bool([]))
+
+print('Bitwise Operators:')
+print("binary value: {0:b}".format(int('100',2)))
+print("right shift:  {0:b}".format(int('100',2)>>1))
+print("left shift:   {0:b}".format(int('100',2)<<1))
+print("bitwise and:  {0:b}".format(int('11111111', 2) & int('10101010', 2)))
+print("bitwise or:   {0:b}".format(int('11111111', 2) | int('10101010', 2)))
+print("bitwise xor:  {0:b}".format(int('11111111', 2) ^ int('10101010', 2)))
+print("-value-a:     {0:b}".format(~ int('10101010', 2)))
 
 print('Membership Operators:')
 mySet = ['doctor', 'engineer', 'farmer', 'worker']
