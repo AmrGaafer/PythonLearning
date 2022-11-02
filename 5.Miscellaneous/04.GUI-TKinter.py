d2ef0ce34728a5ef28c3cfb2538a48cf1402ff55
AmrAwad88 <awad.amr1988@gmail.com>
2022-11-02 09:50:27 +0100

    Button() object

--- 5.Miscellaneous/04.GUI-TKinter.py
+++ 5.Miscellaneous/04.GUI-TKinter.py
@@ -1,5 +1,20 @@
 # -------------------------------------------------------------------------------------------------------------
-# TKinter
+# TKinter:
+#   The concept is to have a GUI environment and create widgets within that environment
+#
+# TK()      returns a GUI environment   (creating a GUI)
+#
+# Label()   creates a GUI label element (creating a GUI element)
+#   labelElement.pack()                 packs that element on the GUI
+#   labelElement.grid(row= , column=)   puts that element on the GUI at the given grid positions
+#                                       (The zero position is the upper left position,
+#                                       the given positions are relative)
+#   NOTE: The two previous methods could not be called on the same GUI environment
+#
+# Button()  creates a GUI button element
+#           - padx, pady: width and height
+#           - command: the funciton to be called on click (name is given without paranthesis)
+#           - fg, bg: text and background color
 # -------------------------------------------------------------------------------------------------------------
 
 import os
@@ -7,11 +22,26 @@ os.system('cls')        # cls command
 
 from tkinter import *
 
+# creating a GUI object
 root = Tk()
 
-# creating a label widget
-myLabel = Label(root, text = 'Hello World!')
-# putting the label on the screen
-myLabel.pack()
+# creating a label widget (step#1)
+myLabell = Label(root, text = 'Hello World!')
+myLabel2 = Label(root, text = 'My name is Amr')
+myLabel3 = Label(root, text = 'I love Python')
+
+# putting the label on the screen (step#2)
+myLabell.grid(row= 0, column=0)
+myLabel2.grid(row= 1, column=1)
+myLabel3.grid(row= 2, column=0)
+
+
+def myClick():
+    myLabel = Label(root, text = 'Horraaaa!').grid(row=5, column=0)
+
+# creating a button widget (step#1)
+myButton = Button(root, text='Click me!', padx= 50, pady= 50, command=myClick, fg= 'blue', bg= 'white')
+# putting the button on the screen (step#2)
+myButton.grid(row= 4, column=4)
 
 root.mainloop()
