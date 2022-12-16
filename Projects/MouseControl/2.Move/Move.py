6babff6fb246849413120f18cbfb450c7963e837
AmrAwad88 <awad.amr1988@gmail.com>
2022-12-16 11:10:49 +0100

    test version on VM

--- Projects/MouseControl/2.Move/Move.py
+++ Projects/MouseControl/2.Move/Move.py
@@ -17,6 +17,7 @@ os.system('cls')        # cls command
 import time
 
 # maximize screen and focus:
+
 # solution #1:
 #import win32gui, win32com
 #hwnd = pywin32_system32.GetForegroundWindow()
@@ -41,24 +42,41 @@ import pyfiglet
 #print(pygetwindow.getAllTitles())   # get all the open windows
 try:
     scope = pygetwindow.getWindowsWithTitle('Scope')[0]
-    print(scope)
+    #print(scope)
     scope.minimize()
     scope.maximize()
 except:
-    print('Press Ctrl-C to quit.')
+    print('zum Beenden Strg-C drücken.')
     print(termcolor.colored(pyfiglet.figlet_format('ScopeView Projekt ist nicht offen'), color='red'))
     while True:
         continue
 
 
-# move mouse and click:
+# move mouse and click
 import mouse
+xMin, xMax = 10, 1910
+yMin, yMax = 10, 1070
 
-mouse.move(10, 10, absolute= True, duration= 1)
-time.sleep(1)
-mouse.move(1910, 10, absolute= True, duration= 1)
-time.sleep(1)
-mouse.move(1910, 1000, absolute= True, duration= 1)
-mouse.right_click()
-time.sleep(1)
-mouse.move(10, 1070, absolute= True, duration= 1)
+xCollabseAll, yCollabseAll = 165, 145
+xExpand, yExpand = 20, 210
+xProject, yProject = 110, 230
+xStartRecord, yStartRecord = 1305, 65
+
+# dummy mouse motion 
+mouse.move(xMin, yMin, absolute= True, duration= 1)
+mouse.move(xMax, yMin, absolute= True, duration= 1)
+mouse.move(xMax, yMax, absolute= True, duration= 1)
+mouse.move(xMin, yMax, absolute= True, duration= 1)
+
+# scope project recort start
+mouse.move(xCollabseAll, yCollabseAll, absolute= True, duration= 1)
+mouse.click()
+mouse.move(xExpand, yExpand, absolute= True, duration= 1)
+mouse.click()
+mouse.move(xProject, yProject, absolute= True, duration= 1)
+mouse.click()
+time.sleep(2)
+mouse.move(xStartRecord, yStartRecord, absolute= True, duration= 1)
+mouse.click()
+time.sleep(5)
+scope.minimize()
