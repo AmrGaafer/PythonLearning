6fb327dcf03156d76a648c7cf7e90a698a9364b8
AmrAwad88 <awad.amr1988@gmail.com>
2022-12-23 18:11:35 +0100

    sorting files

--- 3.Functions/Exercices/Exercise19.py
+++ 3.Functions/Exercices/Exercise19.py
@@ -1,7 +1,7 @@
 # -------------------------------------------------------------------------------------------------------------
-# Exercise #19-1:   Generators & Decorators
-# Developer:        Amr Gaafer
-# Date:             29.09.2022
+# Exercise #19: Generators & Decorators
+# Developer:    Amr Gaafer
+# Date:         29.09.2022
 # -------------------------------------------------------------------------------------------------------------
 
 import os
@@ -13,6 +13,25 @@ def reverse_string(my_string):
     for character in my_string[::-1]:
         yield character
 
-# Test
 for c in reverse_string("Elzero"):
     print(c)
+
+print("Task#2: Decorators:")
+
+def myDecorator(fn):
+    def nested_fn():
+        print('Sugar Added From Decorators')
+        fn()
+        print('#'*20)
+    return nested_fn
+
+@myDecorator
+def make_tea():
+    print("Tea Created")
+
+@myDecorator
+def make_coffe():
+    print("Coffe Created")
+
+make_tea()
+make_coffe()
