cbd2df57f01a29519ca83a20ef35cb9e88b53785
AmrAwad88 <awad.amr1988@gmail.com>
2023-01-05 16:44:57 +0100

    string module

---
+++ 5.Modules/03.string.py
@@ -0,0 +1,28 @@
+# --------------------------------------------------------------------------------------------------
+# string:
+#   module that holds predefined strings
+# --------------------------------------------------------------------------------------------------
+
+import os
+import string           # string module
+import random
+os.system('cls')        # cls command
+
+print('string module:')
+
+print(dir(string))
+
+print(string.ascii_letters)     # abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
+print(string.ascii_lowercase)   # abcdefghijklmnopqrstuvwxyz
+print(string.ascii_uppercase)   # ABCDEFGHIJKLMNOPQRSTUVWXYZ
+print(string.capwords)          # function capwords
+print(string.digits)            # 0123456789
+print(string.hexdigits)         # 0123456789abcdefABCDEF
+print(string.octdigits)         # 01234567
+print(string.printable)         # 0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+print(string.punctuation)       # !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+print(string.whitespace)
+
+# serial code generator
+nr_letters = int(input('Enter an integer number defining the length of the serial number: '))
+print(''.join(random.choice(string.ascii_letters+string.digits) for i in range(0, nr_letters)))
