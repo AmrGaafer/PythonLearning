e7bad20cbc173fa934a6d7ad139550f7e87af801
AmrAwad88 <awad.amr1988@gmail.com>
2023-01-10 10:03:46 +0100

    hello_world in snake_case

---
+++ 1.BasicSyntax&DataTypes/01.hello_world.py
@@ -0,0 +1,27 @@
+# --------------------------------------------------------------------------------------------------
+# information about the file:
+#   1. License
+#   2. Who created the file
+#   3. When the file was created
+#   4. Why the file was created
+#   5. Comment is written before or beside the programming line
+#   Important: The shortcut to comment a line is Ctrl+?
+# --------------------------------------------------------------------------------------------------
+
+"""
+This
+is
+not a
+Multiple-line
+Comment
+"""
+
+import os
+os.system('cls')        # cls command
+
+print("Hello World")
+
+os.system('python --version')   # Python executable of Python installation
+os.system('py --version')       # Python launcher, utility that comes with Python installation
+
+# print("Comment me out!")    # This is an inline comment
